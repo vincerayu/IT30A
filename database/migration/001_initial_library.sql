18236a809a6f6c0dcf67202b6123f1250ce74af1
Vince Halinon <halinonv@gmail.com>
2026-09-08 13:09:29 +0800

    added database\migration and 001_initial_library.sql

---
+++ database/migration/001_initial_library.sql
@@ -0,0 +1,98 @@
+-- Table #1 students table
+CREATE TABLE IF NOT EXISTS students (
+    -- Primary key for the students table
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    -- Student name
+    student_first_name VARCHAR (50) NOT NULL,
+    student_last_name VARCHAR (50) NOT NULL,
+
+    -- Student course
+    student_course VARCHAR (50) NOT NULL,
+
+    -- Student Ccreated at timestamp
+    student_created_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+
+)   ENGINE=InnoDB
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_general_ci;
+
+-- Table #2 books
+
+CREATE TABLE IF NOT EXISTS books (
+    -- Primary key for books table
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    -- Book details
+    book_title VARCHAR (50) NOT NULL,
+    book_author VARCHAR (100) NOT NULL,
+    book_category VARCHAR(50) NOT NULL,
+
+    -- Book created at timestamp
+    book_created_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+
+)   ENGINE=InnoDB
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_general_ci;
+
+-- Table #3 borrow
+CREATE TABLE IF NOT EXISTS borrow (
+    -- Primary key for borrow table
+    borrow_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    -- Foreign key references
+    student_id INT NOT NULL,
+    book_id INT NOT NULL,
+
+    -- Borrow timestamp not null by default
+    borrow_date TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    -- Borrow return timestamp null by default 
+    borrow_return_date TIMESTAMP NULL
+        DEFAULT NULL,
+    
+    -- Borrow table constraints and foreign keys
+    CONSTRAINT fk_borrow_student
+        FOREIGN KEY (student_id)
+        REFERENCES students (student_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+
+    CONSTRAINT fk_borrow_book
+        FOREIGN KEY (book_id)
+        REFERENCES books (book_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+
+)   ENGINE=InnoDB
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_general_ci;
+
+INSERT INTO students (
+    student_first_name,
+    student_last_name,
+    student_course
+) VALUES
+    ('VINCE RYU', 'HALINON', 'BSIT'),
+    ('ZUSTIN', 'ANASCO', 'BSIT'),
+    ('JINREX', 'LEDESMA', 'BSCS');
+
+INSERT INTO books (
+    book_title,
+    book_author,
+    book_category
+) VALUES
+    ("Hardcore Leveling Warrior", "Kim Seehon", "Action-Adventure"),
+    ("Chainsawman", "Tatsumaki Fujimoto", "Shonen"),
+    ("Naruto", "Misashi Kishimoto", "Shonen");
+
+INSERT INTO borrow (
+    student_id,
+    book_id
+) VALUES
+    (1, 2),
+    (2, 1),
+    (3, 3);
